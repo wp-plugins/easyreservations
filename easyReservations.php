1b920b371986257971f5771d0e5fc81f91d98a05
feryaz <feryaz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-05 08:08:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/easyreservations/tags/1.3.1@514490 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ easyReservations.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Plugin Name: easyReservations
+Plugin URI: http://www.feryaz.com
+Description: easyReservation is a Reservations or Booking Plugin for Websites with rentable content. It grants you a fast, structured and detailed overview of your Reservations. For help read the Dokumentation on the Pulgin Page.
+Version: 1.3.1
+Author: Feryaz Beer
+Author URI: http://www.feryaz.com
+License:GPL2
+*/
+
+	define('RESERVATIONS_STYLE', get_option("reservations_style"));
+	define('RESERVATIONS_IMAGES_DIR', WP_PLUGIN_URL.'/easyreservations/images');
+	define('RESERVATIONS_LIB_DIR', WP_PLUGIN_URL.'/easyreservations/lib/');
+	define('RESERVATIONS_JS_DIR', WP_PLUGIN_URL.'/easyreservations/js');
+
+	if(is_admin()){
+
+		require_once(dirname(__FILE__)."/lib/functions/admin.php");
+		
+		require_once(dirname(__FILE__)."/easyReservations_admin_main.php");
+
+		require_once(dirname(__FILE__)."/easyReservations_admin_resources.php");
+
+		require_once(dirname(__FILE__)."/easyReservations_admin_statistics.php");
+
+		require_once(dirname(__FILE__)."/easyReservations_admin_settings.php");
+
+	} else {
+
+		require_once(dirname(__FILE__)."/lib/functions/front.php");
+
+		require_once(dirname(__FILE__)."/easyReservations_form_shortcode.php");
+
+		require_once(dirname(__FILE__)."/easyReservations_edit_shortcode.php");
+
+		require_once(dirname(__FILE__)."/easyReservations_calendar_shortcode.php");
+
+		add_shortcode('easy_calendar', 'reservations_calendar_shortcode');
+		add_shortcode('easy_edit', 'reservations_edit_shortcode');
+		add_shortcode('easy_form', 'reservations_form_shortcode');
+
+	}
+
+	require_once(dirname(__FILE__)."/lib/functions/both.php");
+	require_once(dirname(__FILE__)."/lib/widgets/form_widget.php");
+	if(file_exists(dirname(__FILE__).'/lib/plugins/paypal/paypal.php')){
+		require_once(dirname(__FILE__)."/lib/plugins/paypal/paypal.php");
+	}
+
+
+?>
---
+++ js/ajax/send_calendar.js
@@ -0,0 +1,52 @@
+function generateXMLHttpReqObj(){
+  var resObjekt = null;
+  try {
+    resObjekt = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+  catch(Error){
+    try {
+      resObjekt = new ActiveXObject("MSXML2.XMLHTTP");
+    }
+    catch(Error){
+      try {
+      resObjekt = new XMLHttpRequest();
+      }
+      catch(Error){
+        alert("AJAX error");
+      }
+    }
+  }
+  return resObjekt;
+}
+function generateAJAXObjekt(){
+  this.generateXMLHttpReqObj = generateXMLHttpReqObj;
+}
+iol = new generateAJAXObjekt();
+resObjekt = iol.generateXMLHttpReqObj();
+
+function easyRes_sendReq_Calendar() {
+	if(document.getElementById('urlCalendar').type == "hidden") var url = document.getElementById('urlCalendar').value;
+	else var url = document.getElementById('urlCalendar').src;
+
+		if(document.CalendarFormular.date.value !=""){
+			resObjekt.open('post', url.replace("send_calendar.js", "") + 'send_calendar.php' ,true);
+			resObjekt.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+			resObjekt.onreadystatechange = handleResponseCal;
+			
+			if(document.CalendarFormular.childs === undefined) var childs = '';
+			else var childs = '&childs=' + document.CalendarFormular.childs.value;
+			if(document.CalendarFormular.persons === undefined) var persons = '';
+			else var persons = '&persons=' + document.CalendarFormular.persons.value;
+			if(document.CalendarFormular.reservated === undefined) var reservated = '';
+			else var reservated = '&reservated=' + document.CalendarFormular.reservated.value;
+			resObjekt.send('date=' + escape(document.CalendarFormular.date.value) + '&room=' + escape(document.CalendarFormular.room.value) +  '&offer=' + escape(document.CalendarFormular.offer.value) + '&size=' + escape(document.CalendarFormular.size.value) + childs + persons + reservated);
+		}
+}
+
+function handleResponseCal() {
+	var text="";
+  if(resObjekt.readyState == 4){
+  	text=resObjekt.responseText;
+    document.getElementById("showCalender").innerHTML = text;
+  }
+}
